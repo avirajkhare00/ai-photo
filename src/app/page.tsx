c5ef0a5a068d8c44f976504ea2a66400b627854b
avirajkhare00 <avirajkhare00@gmail.com>
2023-11-12 02:13:10 +0530

    [Aviraj] Updated Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<main className="font-sans bg-gray-100">
+		<div className="font-sans bg-gray-100">
 			<header className="text-gray-600 body-font">
 				<div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 					<nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
@@ -66,6 +66,6 @@ export default function Home() {
 					</p>
 				</div>
 			</footer>
-		</main>
+		</div>
 	)
 }

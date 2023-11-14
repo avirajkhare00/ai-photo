bc47878d98711b8718bb5963241273d3bb36f286
Aviraj Khare <49766964+avirajkhare00@users.noreply.github.com>
2023-11-14 22:30:30 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
 			<footer className="text-gray-600 body-font">
 				<div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
 					<p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:py-2 sm:mt-0 mt-4">
-						© 2023 ColorMint —
+						© 2023 ColorMint — Jazzz
 						<a href="#" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@ColorMint</a>
 					</p>
 				</div>
